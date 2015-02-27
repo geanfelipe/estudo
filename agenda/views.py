ab648539b3f72f1840ac07398e3f971ed645ea63
Gean Felipe <geanfelipemoura@outlook.com>
2015-02-27 02:08:01 -0300

    login feito

--- agenda/views.py
+++ agenda/views.py
@@ -2,9 +2,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse,request, HttpResponseRedirect, HttpRequest
 from django.template.response import TemplateResponse
+from django.contrib.auth import authenticate
 
 
 def index(request):
 	response = HttpResponse("Aqui esta o texto web")
 	response.write("<h2> <a href='/login'>Login</h2>")
 	return response
+
+def login(request):
+	usuario = authenticate(username='gean', password='123')
+	response = HttpResponse()
+	if usuario is not None:
+		if usuario.is_active:
+			response.write("Usuario valido")
+		else:
+			reponse.write("Usuario nao ativo")
+	else:
+		response.write("Algo incorreto")
+
+	return response
+			
--- teste/urls.py
+++ teste/urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', agenda.views.index, name = "index"),
+    url(r'^login/', agenda.views.login, name= 'login'),
 
 
 )
