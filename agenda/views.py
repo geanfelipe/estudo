b9381991bbffc3b81bbb6aabb3b3965119c8f0b0
Gean Felipe <geanfelipemoura@outlook.com>
2015-02-27 02:17:36 -0300

    se o usuario eh valido

--- agenda/views.py
+++ agenda/views.py
@@ -1,25 +1,28 @@
-# -*- condig: utf-8 -*-
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse,request, HttpResponseRedirect, HttpRequest
 from django.template.response import TemplateResponse
 from django.contrib.auth import authenticate
-
+from django.contrib.auth.models import User
 
 def index(request):
 	response = HttpResponse("Aqui esta o texto web")
-	response.write("<h2> <a href='/login'>Login</h2>")
+	response.write("<h2> <a href='/usuariovalido'>Sou valido</h2>")
 	return response
 
-def login(request):
+def usuarioValido(request):
 	usuario = authenticate(username='gean', password='123')
+
 	response = HttpResponse()
 	if usuario is not None:
 		if usuario.is_active:
-			response.write("Usuario valido")
+			response.write("Usuario gean é valido")
 		else:
 			reponse.write("Usuario nao ativo")
 	else:
 		response.write("Algo incorreto")
 
 	return response
-			
+
+def login(request):
+	usuario = User.
--- teste/urls.py
+++ teste/urls.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
@@ -10,7 +11,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', agenda.views.index, name = "index"),
-    url(r'^login/', agenda.views.login, name= 'login'),
+    url(r'^usuariovalido/', agenda.views.usuarioValido, name = 'usuarioValido')
+#    url(r'^login/', agenda.views.login, name= 'login'),
 
 
 )
